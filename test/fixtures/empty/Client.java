b1fc0c1ac5c512a6b8cade52e8e761604354fc40
sonny <wb-szq493230@alibaba-inc.com>
2020-06-19 16:17:14 +0800

    improve java generator to be compatible with darafile

--- test/fixtures/empty/Client.java
+++
@@ -1,9 +0,0 @@
-// This file is auto-generated, don't edit it. Thanks.
-package com.aliyun.test;
-
-import com.aliyun.tea.*;
-
-import com.aliyun.test.BaseClient;
-
-public class Client extends BaseClient {
-}
