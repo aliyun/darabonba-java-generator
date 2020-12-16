58809c65175df2559008c5fa44126e0192097b21
yndu13 <yndu13@gmail.com>
2020-12-16 15:26:45 +0800

    optimize visitSetMethod

---
+++ test/fixtures/set/Client.java
@@ -0,0 +1,16 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.test;
+
+import com.aliyun.tea.*;
+import com.aliyun.test.models.*;
+
+public class Client {
+
+    public static void testSet() throws Exception {
+        Test m = new Test()
+                .setField1("test1")
+                .setField2("test2")
+                .setField3("test3")
+                .setField4("test4");
+    }
+}
