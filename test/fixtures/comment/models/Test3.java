2cbc6ed9b4db4322a9ceb1b9d278d79a8a2999cd
yndu13 <yndu13@gmail.com>
2022-12-12 23:43:10 +0800

    support java doc with annotation and comments

---
+++ test/fixtures/comment/models/Test3.java
@@ -0,0 +1,17 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.test.models;
+
+import com.aliyun.tea.*;
+
+/**
+  TestModel3
+*/
+public class Test3 extends TeaModel {
+    // empty comment1
+    // empy comment2
+    public static Test3 build(java.util.Map<String, ?> map) throws Exception {
+        Test3 self = new Test3();
+        return TeaModel.build(map, self);
+    }
+
+}
