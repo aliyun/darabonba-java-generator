e359375f0d843b35d0c3840431966071ff4c22c8
飞澋 <zoupanpan.zpp@alibaba-inc.com>
2025-01-10 10:43:07 +0800

    add string in builtin

--- test/fixtures/builtin/Client.java
+++ test/fixtures/builtin/Client.java
@@ -15,15 +15,15 @@ public class Client {
                 new TeaPair("key6", "321")
             ))
         );
-        String ms = JSONUitls.stringify(m);
-        Object ma = JSONUitls.parseJSON(ms);
+        String ms = JSONUtils.stringify(m);
+        Object ma = JSONUtils.parseJSON(ms);
         String arrStr = "[1,2,3,4]";
-        Object arr = JSONUitls.parseJSON(arrStr);
-        Object res = JSONUitls.readPath(m, "$.key1.key2");
+        Object arr = JSONUtils.parseJSON(arrStr);
+        Object res = JSONUtils.readPath(m, "$.key1.key2");
     }
 
     public static void loggerTest(java.util.List<String> args) throws Exception {
-        System.out.println("test");
+        // $Logger.trace("test");
         System.out.println("test");
         System.out.println("test");
         System.out.println("test");
@@ -34,17 +34,50 @@ public class Client {
         String es = System.getenv("TEST");
     }
 
+    public static void stringTest(java.util.List<String> args) throws Exception {
+        String fullStr = "cn-hangzhou, cn-shanghai";
+        if (.equals("cn-hangzhou")) {
+            args = fullStr.split(",");
+        }
+
+        if (fullStr.contains("hangzhou")) {
+            Integer length = fullStr.length();
+        }
+
+        if (fullStr.startsWith("cn-")) {
+        }
+
+        if (fullStr.endsWith("cn-")) {
+            Integer start = fullStr.indexOf("hangzhou");
+            Integer end = start + 7;
+            String region = fullStr.substring(start, end);
+            String lowerRegion = region.toLowerCase();
+            String upperRegion = region.toUpperCase();
+        }
+
+        String em = "xxx";
+        if (em.isEmpty()) {
+            return ;
+        }
+
+        String num = "32.01";
+        Integer inum = Integer.parseInt(num) + 3;
+        Long lnum = Long.parseLong(num);
+        Float fnum = Float.parseFloat(num) + 1F;
+        Double dnum = Double.parseDouble(num) + 1D;
+    }
+
     public static void main(String[] args_) throws Exception {
         java.util.List<String> args = java.util.Arrays.asList(args_);
         Integer a = 1;
-        String b = (String)(a);
-        Integer f = (Integer)(b);
-        Long g = (Long)(b);
-        Long h = (Long)(b);
-        Float n = (Float)(b);
-        Double o = (Double)(b);
-        if ((Boolean)(args.get(2))) {
-            String data = (String)(args.get(2));
+        String b = a.toString();
+        Integer f = ConverterUtils.parseInt(b);
+        Long g = ConverterUtils.parseInt(b);
+        Long h = ConverterUtils.parseLong(b);
+        Float n = ConverterUtils.parseFloat(b);
+        Double o = Double.parseDouble(b.toString());
+        if (ConverterUtils.parseBoolean(args.get(2))) {
+            String data = args.get(2).toString();
             Object test = data;
             java.util.Map<String, String> maps = TeaConverter.buildMap(
                 new TeaPair("key", "value")
@@ -56,8 +89,8 @@ public class Client {
             return ;
         }
 
-        Thread.Sleep(a);
-        String defaultVal = (String)(args.get(0) == null ? args.get(0) : args.get(1));
+        Thread.sleep(a);
+        String defaultVal = args.get(0) == null ? args.get(0) : args.get(1).toString();
         if (defaultVal == b) {
             return ;
         }
